b4452b9775de5ae6498acff78e9299eb2e04661f
kewiany <kewin.czuprynski@gmail.com>
2019-11-22 00:24:24 +0100

    Import classes to use, create first sample intent with response and suggestion

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const functions = require('firebase-functions');
+const {
+  dialogflow,
+  BasicCard,
+  BrowseCarousel,
+  BrowseCarouselItem,
+  Button,
+  Carousel,
+  Image,
+  LinkOutSuggestion,
+  List,
+  MediaObject,
+  Suggestions,
+  SimpleResponse,
+ } = require('actions-on-google');
+const app = dialogflow({
+    debug : true
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+app.intent('ABC', (conv, params) => {
+    console.log("Starting with `what do you want to know?`");
+    conv.ask(new SimpleResponse({
+       speech:'Sorry, I do not know,
+       text: 'Sorry, I do not know,
+    }));
+   conv.ask(new Suggestions(['Help me', 'Cancel',' Quit']));
+});
