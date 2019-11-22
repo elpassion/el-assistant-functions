fa87d2ffb95800f9bc8574b35bf0eb44c1ef057f
kewiany <kewin.czuprynski@gmail.com>
2019-11-22 12:41:33 +0100

    Add firebase

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const firebase = require("firebase/app");
+require("firebase/firestore");
+
+var firebaseConfig = {
+   apiKey: "AIzaSyAfJKotm2CJllKceY7DTQtDTTh5ejnMjEA",
+   authDomain: "el-assistant.firebaseapp.com",
+   databaseURL: "https://el-assistant.firebaseio.com",
+   projectId: "el-assistant",
+   storageBucket: "el-assistant.appspot.com",
+   messagingSenderId: "1024141565851",
+   appId: "1:1024141565851:web:b4462fae24ba210080cb3e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const functions = require('firebase-functions');
 const {
   dialogflow,
@@ -15,6 +30,7 @@ const {
   Suggestions,
   SimpleResponse,
  } = require('actions-on-google');
+
 const app = dialogflow({
     debug : true
 });
@@ -24,8 +40,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 app.intent('ABC', (conv, params) => {
     console.log("Starting with `what do you want to know?`");
     conv.ask(new SimpleResponse({
-       speech:'Sorry, I do not know,
-       text: 'Sorry, I do not know,
+       speech:'Sorry, I do not know`,
+       text: 'Sorry, I do not know`,
     }));
    conv.ask(new Suggestions(['Help me', 'Cancel',' Quit']));
 });
